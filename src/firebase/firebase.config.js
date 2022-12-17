8afc9e4b24649efc0c507f8ea0bbbc93c21fecc6
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-17 20:05:01 +0600

    initial setup and install firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8JPGS1xWXfLr9Oa3KUV5w1C8UFOa77_Y",
+  authDomain: "geowild-photography.firebaseapp.com",
+  projectId: "geowild-photography",
+  storageBucket: "geowild-photography.appspot.com",
+  messagingSenderId: "211051285268",
+  appId: "1:211051285268:web:d3a73ae3aa5df07ebf9001",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
