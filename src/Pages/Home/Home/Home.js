37cb34f38740a8df323154d8ced165bc00d395d7
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-18 10:45:43 +0600

    added awesome slider

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,7 +1,9 @@
+import Slider from "../Slider/Slider";
+
 const Home = () => {
   return (
     <div>
-      <h1>This is home</h1>
+      <Slider />
     </div>
   );
 };
--- src/Pages/Shared/MenuBar/MenuBar.js
+++ src/Pages/Shared/MenuBar/MenuBar.js
@@ -1,7 +1,23 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const MenuBar = () => {
   return (
     <div>
-      <h1>this is navbar</h1>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
